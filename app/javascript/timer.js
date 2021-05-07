57bef6ad9a23431af86f7a42b165efc11fdb087b
yakyutarou <78419230+yakyutarou@users.noreply.github.com>
2021-05-07 17:21:54 +0900

    Jacascript1

---
+++ app/javascript/timer.js
@@ -0,0 +1,27 @@
+/**  
+ * タイマーを開始する
+ */
+function startTimer(){
+
+} 
+
+/**
+ * タイマーを終了する
+ */
+function stopTimer(){
+
+}
+
+/**
+ * 残り時間をチェックする繰り返し
+ */
+function checkRemainingTime(){
+
+}
+
+/**
+ * 残り時間を表示する
+ */
+function setDisplay(second){
+  
+}
