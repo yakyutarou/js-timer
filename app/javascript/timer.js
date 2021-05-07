68ce61c47be6cd8590547707bd7e24e53e10d07e
yakyutarou <78419230+yakyutarou@users.noreply.github.com>
2021-05-07 17:40:58 +0900

    Javascript2

--- app/javascript/timer.js
+++ app/javascript/timer.js
@@ -1,8 +1,13 @@
 /**  
  * タイマーを開始する
  */
-function startTimer(){
 
+let finish 
+let intervalId
+function startTimer(){
+  let second = document.querySelector("#time-input").value
+  finish = Date.now() + second * 1000
+  intervalId = setInterval(checkRemainingTime,50)
 } 
 
 /**
@@ -16,12 +21,17 @@ function stopTimer(){
  * 残り時間をチェックする繰り返し
  */
 function checkRemainingTime(){
-
+  let remain = finish - Date.now()
+  //残り時間が０以下になったらタイマーを終了する
+  if (remain <= 0){
+    stopTimer()
+    alert("時間になりました")
+  }
 }
 
 /**
  * 残り時間を表示する
  */
 function setDisplay(second){
-  
+
 }
